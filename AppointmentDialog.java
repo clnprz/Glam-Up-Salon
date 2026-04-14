1fb6d08c1a48770b19b92aee63315bfa3f3ea79b
Clysce <162132948+clnprz@users.noreply.github.com>
2026-04-14 23:50:31 +0800

    dialog classes

---
+++ AppointmentDialog.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dialogs;
+
+import java.awt.Frame;
+import ui.Login;
+
+/**
+ *
+ * @author Jasmine
+ */
+public class AppointmentDialog extends javax.swing.JDialog {
+
+    public AppointmentDialog(Frame frame, boolean b) {
+    }
+
+    public void setVisible(boolean b) {
+        // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
